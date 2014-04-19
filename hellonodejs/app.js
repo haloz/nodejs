c9a3d522bdb2f9b48e940659cf758835cabb3194
Sebastian <haloz@gmx.net>
2014-04-19 20:38:31 +0200

    socket chat

--- hellonodejs/app.js
+++ hellonodejs/app.js
@@ -1,7 +1,24 @@
 var net = require("net");
+
+var sockets = [];
 var server = net.createServer(function(socket) {
+	sockets.push(socket);
 	socket.write('hallo');
 	socket.write('welt!');
 	socket.write("\n");
-	socket.end();
+
+	socket.on('data', function(data) {
+		console.log("sending data to "+sockets.length+" sockets...");
+		for(var i = 0, iLength = sockets.length; i < iLength; i++) {
+			if(sockets[i] !== socket) {
+				sockets[i].write(data);
+			}
+		}
+	});
+
+	socket.on('end', function(data) {
+		sockets.splice(sockets.indexOf(socket));
+	});
+
+	//socket.end();
 }).listen(3000);
