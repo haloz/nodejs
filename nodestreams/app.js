bd7e2d690db7dacebae07ed2b256c7da3be8bcd9
Sebastian <haloz@gmx.net>
2014-05-05 18:55:04 +0200

    for example you can use gzip as a pipe in between: the stream reader reads the file into the pipe, passes this data to the gzip module and the gzip passes it to the http response

--- nodestreams/app.js
+++ nodestreams/app.js
@@ -1,9 +1,14 @@
 var fs = require("fs"),
 	http = require("http"),
+	zlib = require("zlib"),
 	path = require("path");
 
 var server = http.createServer(function(req, res) {
-	var readableStream = fs.createReadStream(path.join(__dirname, req.url));
-	readableStream.pipe(res);
+	res.writeHead(200, {
+		"Content-Encoding" : "gzip"
+	});
+	var readableStream = fs.createReadStream(path.join(__dirname, req.url))
+		.pipe(zlib.createGzip())
+		.pipe(res);
 });
 server.listen(8080);
