7d032ea2f7b309734189d57a23bf057028527959
Sebastian <haloz@gmx.net>
2014-05-05 23:17:35 +0200

    implement extra transform stream to convert the floating point numbers into strings - which then can be piped to the http response

--- nodestreams/primes.js
+++ nodestreams/primes.js
@@ -2,6 +2,12 @@ var util = require("util"),
 	stream = require("stream"),
 	http = require("http");
 
+
+/*
+	Prime
+	A simple read stream that generates prime numbers
+	outputs the stream data as floating point data (not as string!)
+*/
 var isPrime = function(n) {
 	for(var i = 2; i <= Math.sqrt(n); i++) {
 		if(n % i == 0) {
@@ -12,10 +18,11 @@ var isPrime = function(n) {
 };
 
 var Readable = stream.Readable;
+var Transform = stream.Transform;
 
 var Primes = function(max, options) {
 	options = options || {};
-	//options.objectMode = true;
+	options.objectMode = true;
 	Readable.call(this, options); // basiskonstruktor 
 	this._max = max;
 	this._index = 2;
@@ -32,19 +39,51 @@ Primes.prototype._read = function() {
 		}
 		if(isPrime(this._index)) {
 			foundPrime = true;
-			/*
-				Note: method 1: return floating point numbers, but then the app that uses the stream
-				has to convert the data e.g. into a string.
-				method 2: store stings of the numbers so we can directly use .pipe()
-			*/
-			this.push(this._index.toString());
+			this.push(this._index);
 		}
 		this._index++;
 	} while (!foundPrime);
 };
 
+
+
+/*
+	Stringifier
+	A basic transform stream. This simply takes the raw/object data and applies a toString
+	outputs the stringified values as stream
+*/
+var stringify = function(n) {
+	return n.toString();
+};
+
+var Stringifier = function(options) {
+	options = options || {};	
+	 /*
+	 	this is important! Without this the stream would be in "string" mode,
+	 	allowing only strings as input. For raw/object data this parameter has to be set.
+	 */
+	options.objectMode = true;
+	Transform.call(this, options);
+};
+
+util.inherits(Stringifier, Transform);
+
+Stringifier.prototype._transform = function(chunk, encoding, callback) {
+	var stringified = stringify(chunk);
+	this.push(stringified, encoding);	
+	callback();
+};
+
+
+
+
+
+
 // run
 var server = http.createServer(function(req, res) {
-	var primesStream = new Primes(1000);
-	primesStream.pipe(res);
+	var primesStream = new Primes(1000); 
+	var stringifyStream = new Stringifier(); 
+	primesStream					// data source
+		.pipe(stringifyStream)		// float to string
+		.pipe(res);					// target
 }).listen(8080);
